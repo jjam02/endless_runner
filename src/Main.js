9beb454e19b7c759495b55fb530bc1c42ce0594f
Jonathan Alvarez <jalva100@ucsc.edu>
2022-04-25 16:02:43 -0700

    menu and play scene

---
+++ src/Main.js
@@ -0,0 +1,31 @@
+
+
+// tame the javashrek
+'use strict';
+
+// global variables
+let keyLeft;
+let keySPACE;
+let currentScene = 0;
+const SCALE = 0.5;
+const tileSize = 35;
+
+// main game object
+let config = {
+    type: Phaser.WEBGL,
+    width: 840,
+    height: 525,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: true,
+            gravity: {
+                x: 0,
+                y: 0
+            }
+        }
+    },
+    scene: [ Menu, Play]
+};
+
+let game = new Phaser.Game(config);
