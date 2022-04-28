3b9450722256205b17569861908c387331e6f577
gavilori <gavilori@ucsc.edu>
2022-04-27 17:55:49 -0700

    added custom font, code cleaning

--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -2,30 +2,49 @@ class Menu extends Phaser.Scene {
     constructor() {
       super("menuScene");
     }
+
+    preload() {
+        this.load.image('city', './assets/bg.png');
+    }
     create() {
         let menuConfig = {
-            fontFamily: 'Courier',
-            fontSize: '28px',
-            backgroundColor: '#F3B141',
-            color: '#843605',
-            align: 'right',
+            fontFamily: 'Alagard',
+            fontSize: '90px',
+            color: '#000000',
+            align: 'center',
             padding: {
                 top: 5,
                 bottom: 5,
             },
             fixedWidth: 0
         }
-        this.add.text(game.config.width/2, game.config.height/2, 'RunPocalypse', menuConfig).setOrigin(0.5);
-        this.add.text(game.config.width/2, game.config.height/2, 'press SPACEBAR to start', menuConfig).setOrigin(0.5);
-        menuConfig.backgroundColor = '#00FF00';
-        menuConfig.color = '#000';
+        let dropshadow = {
+            fontFamily: 'Alagard',
+            fontSize: '90px',
+            color: '#505050',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        this.cityscape = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'city').setOrigin(0);
+
+        this.add.text(game.config.width/2 + 4, game.config.height/4 + 4, 'RunPocalypse', dropshadow).setOrigin(0.5);
+        dropshadow.fontSize = '40px';
+        this.add.text(game.config.width/2 + 2, game.config.height/1.5 + 2, 'Press SPACE to start', dropshadow).setOrigin(0.5);
+
+        this.add.text(game.config.width/2, game.config.height/4, 'RunPocalypse', menuConfig).setOrigin(0.5);
+        menuConfig.fontSize = '40px';
+        this.add.text(game.config.width/2, game.config.height/1.5, 'Press SPACE to start', menuConfig).setOrigin(0.5);
+
         // define keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
-        
     }
+
     update() {
         if (Phaser.Input.Keyboard.JustDown(keySPACE)) {
-            // easy mode
             this.scene.start('playScene');    
           }
 
--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -12,38 +12,38 @@ class Play extends Phaser.Scene {
        
     }
 
-    create() { //define keys
+    create() { // define keys
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         
-        //set some constants
+        // set some constants
         this.JUMP_VELOCITY = -700;
         this.SCROLL_SPEED = 5;
         this.physics.world.gravity.y = 2600; 
 
-        //background
+        // background
         this.cityscape = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'city').setOrigin(0);
         // make collision groups
         this.ground = this.add.group();
         this.power = this.add.group();
-        this.enemy = this.add.group()
+        this.enemy = this.add.group();
 
-        //create player
+        // create player
         this.player = this.physics.add.sprite(50, 450,  'player').setOrigin(.5,.5);
 
-        //create shield on player
+        // create shield on player
         this.trash = this.add.rectangle(50,450,20,20,'yellow');
         this.trash.alpha= 0;
 
-        //make shield item
+        // make shield item
         this.shield = this.physics.add.sprite(-50, 200 ,  'shield').setOrigin(0);
         this.shield.body.allowGravity = false;
         this.power.add(this.shield);
 
-        //player health
+        // player health
         this.p1Health = 1;
 
-        //make ground tiles
+        // make ground tiles
         for(let i = 0; i < game.config.width; i += tileSize) { 
             let groundTile = this.physics.add.sprite(i, game.config.height - tileSize,  'floor').setScale(SCALE).setOrigin(0);
             let groundTile2 = this.physics.add.sprite(i, game.config.height - tileSize+10,  'floor').setScale(SCALE).setOrigin(0);
@@ -56,7 +56,7 @@ class Play extends Phaser.Scene {
         }
         
 
-        //create car obj
+        // create car obj
         this.car = this.physics.add.sprite(game.config.width, 455 ,  'car').setOrigin(0);
         this.hood = this.physics.add.sprite(game.config.width+3, 450 ,  'car').setOrigin(0);
         this.hood.setSize(60,5);
@@ -66,12 +66,12 @@ class Play extends Phaser.Scene {
         this.hood.body.immovable = true;
         this.ground.add(this.hood);
 
-        //meteor obj
+        // meteor obj
         this.meteor =  this.physics.add.sprite(game.config.width, Math.random()*(425-380)+380,  'meteor').setOrigin(0);
         this.meteor.body.allowGravity = false;
         
         // add enemies to group
-        this.enemy = this.add.group()
+        this.enemy = this.add.group();
         this.enemy.add(this.car);
         this.enemy.add(this.meteor);
         
@@ -95,35 +95,61 @@ class Play extends Phaser.Scene {
             this.car_reset();
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
-            this.p1Health--
+            this.p1Health--;
         });
         this.physics.add.collider(this.meteor, this.player, ()=>{
             this.trash.alpha= 0;
             this.meteor_reset();
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
-            this.p1Health--
+            this.p1Health--;
         });
 
         // game over variable 
         this.gameOver = false;
         
 
-        //starting vel for enemy objs
+        // starting vel for enemy objs
         this.car.body.setVelocityX(-300);
         this.hood.body.setVelocityX(-300);
         this.meteor.body.setVelocityX(-400);
+
+        // text config
+        let textConfig = {
+            fontFamily: 'Alagard',
+            fontSize: '40px',
+            color: '#000000',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
+        let dropshadow = {
+            fontFamily: 'Alagard',
+            fontSize: '40px',
+            color: '#505050',
+            align: 'center',
+            padding: {
+                top: 5,
+                bottom: 5,
+            },
+            fixedWidth: 0
+        }
         
 
-        //SCORING
+        // SCORING
         this.score = 0;
-        this.scoreRight = this.add.text(game.config.width/2, 50, "Distance: "+this.score+" mi").setOrigin(0.5);
+        this.scoreRightDropshadow = this.add.text(12, 12, "Distance: "+this.score+" mi", dropshadow).setOrigin(0);
+        this.scoreRight = this.add.text(10, 10, "Distance: "+this.score+" mi", textConfig).setOrigin(0);
         this.time.addEvent({ delay: 2500, callback: this.miles, callbackScope: this, loop: true });
 
         if(highScore>=0){
-            this.add.text(game.config.width/2-200, 50, "High Score: "+highScore+" mi").setOrigin(0.5);
+            this.add.text(game.config.width-8, 12, "High Score: "+highScore+" mi", dropshadow).setOrigin(1,0);
+            this.add.text(game.config.width-10, 10, "High Score: "+highScore+" mi", textConfig).setOrigin(1,0);
 
-        //Powerup spawn
+        // powerup spawn
         this.time.addEvent({ delay: 10000, callback: this.shieldSpawn, callbackScope: this, loop: true });
         }
     }
@@ -143,138 +169,139 @@ class Play extends Phaser.Scene {
 
         // what to do in game runtime
         if(!this.gameOver){
+                
+
+            // this.car.body.setVelocityX(-300);
+            // this.meteor.body.setVelocityX(-400);
+
+
+            //make bg scroll
+            this.cityscape.tilePositionX += this.SCROLL_SPEED;
+            //this.groundTile += this.SCROLL_SPEED;
+
+            //check for player touching ground
+            this.player.onGround = this.player.body.touching.down;
+
+            // FIXME: player position can exceed 50 if ducking while velocity.x > 0
+            if(this.player.onGround){
+                this.jump = 1;
+                this.jumping=false;
+                if(this.player.x<50){
+                    this.player.body.velocity.x += 1;
+                }else{
+                    this.player.body.velocity.x = 0;
+                }
+            }
             
 
-        // this.car.body.setVelocityX(-300);
-        // this.meteor.body.setVelocityX(-400);
 
-
-        //make bg scroll
-        this.cityscape.tilePositionX += this.SCROLL_SPEED;
-        //this.groundTile += this.SCROLL_SPEED;
-
-        //check for player touching ground
-        this.player.onGround = this.player.body.touching.down
-        if(this.player.onGround){
-            this.jump = 1;
-            this.jumping=false;
-            if(this.player.x<50){
-                this.player.body.velocity.x += 1;
-            }else{
-                this.player.body.velocity.x = 0;
+            // jumping controller
+            if(this.jump>0 && Phaser.Input.Keyboard.DownDuration(keySPACE,100)&&!keyDOWN.isDown) {
+                // console.log("I JUMPED POGGERS");
+                this.player.body.setVelocityY(-800);
+                this.jumping=true;
             }
-        }
-        
-
-
-        //jumping controller
-        if(this.jump>0 && Phaser.Input.Keyboard.DownDuration(keySPACE,100)&&!keyDOWN.isDown) {
-            console.log("I JUMPED POGGERS");
-	        this.player.body.setVelocityY(-800);
-            this.jumping=true;
-        }
-        if(this.jumping && keyDOWN.isUp) {
-            console.log("LET GO OF SPCACE");
-	    	this.jump--;
-	    	this.jumping = false;
-	    }
-
-
-        //reset things when they go off screen
-        if(this.car.body.x <-200){
-            console.log("OFFSCREEN XD"); 
-            this.car_reset();
-        }
-        
-        if(this.meteor.body.x <-200||this.meteor.body.velocity.x==0){
-            console.log("OFFSCREEN XD"); 
-            this.meteor_reset();
-        }
-        
-
-        // controlling the sliding
-        if(keyDOWN.isDown){
-            this.player.angle = 90;
-            this.player.setSize(65,40);
-            this.player.setDisplaySize(45,70);
-            if(!this.jumping){
-            this.player.y = 467.5;
-            this.player.body.setVelocityY(0);
+            if(this.jumping && keyDOWN.isUp) {
+                // console.log("LET GO OF SPCACE");
+                this.jump--;
+                this.jumping = false;
             }
-        }
 
 
-        // resetting after done sliding
-        if(Phaser.Input.Keyboard.JustUp(keyDOWN)||this.player.y>game.config.height){
-            this.player.body.setVelocityY(0);
-            this.player.angle = 0;
-            this.player.y = 450;
-            this.player.setSize(40,65);
-            this.player.setDisplaySize(45,70);
-            // this.player.x = 70;
-        }
+            // reset things when they go off screen
+            if(this.car.body.x <-200){
+                // console.log("OFFSCREEN XD"); 
+                this.car_reset();
+            }
+            
+            if(this.meteor.body.x <-200||this.meteor.body.velocity.x==0){
+                // console.log("OFFSCREEN XD"); 
+                this.meteor_reset();
+            }
+            
+
+            // controlling the sliding
+            if(keyDOWN.isDown){
+                this.player.angle = 90;
+                this.player.setSize(65,40);
+                this.player.setDisplaySize(45,70);
+                if(!this.jumping){
+                this.player.y = 467.5;
+                this.player.body.setVelocityY(0);
+                }
+            }
 
 
-        //console.log("X"+this.player.x+" Y "+this.player.y);
-        //console.log(this.car.body.velocity.x);
-        // console.log("shield: "+this.shield.body.velocity.x);
-        // console.log("player health: "+this.p1Health);
-        
+            // resetting after done sliding
+            if(Phaser.Input.Keyboard.JustUp(keyDOWN)||this.player.y>game.config.height){
+                this.player.body.setVelocityY(0);
+                this.player.angle = 0;
+                this.player.y = 450;
+                this.player.setSize(40,65);
+                this.player.setDisplaySize(45,70);
+                // this.player.x = 70;
+            }
 
-        // take of shield if player is hit
-        if(this.p1Health>1){
-            this.trash.x = this.player.x;
-            this.trash.y = this.player.y;
 
-        }
+            //console.log("X"+this.player.x+" Y "+this.player.y);
+            //console.log(this.car.body.velocity.x);
+            // console.log("shield: "+this.shield.body.velocity.x);
+            // console.log("player health: "+this.p1Health);
+            
 
-        //when player health reaches 0 or off screen end the game
-        if(this.p1Health <= 0||this.player.x<-50){
-            this.gameOver = true;
-        }
-    }
+            // take of shield if player is hit
+            if(this.p1Health>1){
+                this.trash.x = this.player.x;
+                this.trash.y = this.player.y;
+
+            }
+
+            //when player health reaches 0 or off screen end the game
+            if(this.p1Health <= 0||this.player.x<-50){
+                this.gameOver = true;
+            }
+        }  
     // console.log("CAR SPEED VROOM: "+this.car.body.velocity.x);
-
-}
-
-
-// updates the score when called every 2.5 seconds
-miles(){
-    this.score +=.5;
-    this.scoreRight.text = "Distance: "+this.score + " mi";
-
-}
-
-
-//spawn the shield
-shieldSpawn(){
-    if(this.p1Health==1&&this.shield.x<0){
-        this.shield.x = game.config.width + 50;
-        this.shield.y =  Math.random()*(455-380)+380
-        this.shield.body.setVelocityX(-200);
-        this.shield.body.setVelocityY(0);
     }
-}
 
 
-// reset the meteor 
-meteor_reset(){
-    this.meteor.x = game.config.width+50;
-    this.meteor.setVelocityY(0);
-    this.meteor.setVelocityX(-1*((Math.random()*(500-400)+400)));
-    this.meteor.y = Math.random()*(425-380)+380;
-
-}
+    // updates the score 
+    miles(){
+        this.score +=.5;
+        this.scoreRight.text = "Distance: "+this.score + " mi";
+        this.scoreRightDropshadow.text = "Distance: "+this.score + " mi";
+    }
 
 
-//reset the car
-car_reset(){
-    this.car.x = game.config.width+50;
-    this.num = -1*((Math.random()*(500-300)+300))
-    this.car.body.setVelocityX(this.num);
-    this.hood.x = game.config.width+55;
-    this.hood.body.setVelocityX(this.num);
+    // spawn the shield
+    shieldSpawn(){
+        if(this.p1Health==1&&this.shield.x<0){
+            this.shield.x = game.config.width + 50;
+            this.shield.y =  Math.random()*(455-380)+380
+            this.shield.body.setVelocityX(-200);
+            this.shield.body.setVelocityY(0);
+        }
+    }
 
-}
+
+    // reset the meteor 
+    meteor_reset(){
+        this.meteor.x = game.config.width+50;
+        this.meteor.setVelocityY(0);
+        this.meteor.setVelocityX(-1*((Math.random()*(500-400)+400)));
+        this.meteor.y = Math.random()*(425-380)+380;
 
     }
+
+
+    //reset the car
+    car_reset(){
+        this.car.x = game.config.width+50;
+        this.num = -1*((Math.random()*(500-300)+300))
+        this.car.body.setVelocityX(this.num);
+        this.hood.x = game.config.width+55;
+        this.hood.body.setVelocityX(this.num);
+
+    }
+
+}
