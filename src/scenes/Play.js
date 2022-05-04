a0f8a31894b3ecd1cbba237eceed5a0cfb1070d2
Jonathan Alvarez <jalva100@ucsc.edu>
2022-05-04 03:25:27 -0700

    sounds and some bug fixes

--- src/scenes/Play.js
+++ src/scenes/Play.js
@@ -4,6 +4,7 @@ class Play extends Phaser.Scene {
     }
     preload() {
         this.load.image('city', './assets/bg.png');
+        this.load.image('city2', './assets/bg2.png');
         this.load.image('player','./assets/player.png')
         this.load.image('floor','./assets/floor.png')
         this.load.image('car','./assets/car.png')
@@ -19,13 +20,14 @@ class Play extends Phaser.Scene {
         this.shield_off = this.sound.add('off');
         this.duck = this.sound.add('duck');
         this.slide = this.sound.add('slide');
-        this.bgm = this.sound.add('music');
+        //this.bgm = this.sound.add('music');
         keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
         keyDOWN = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN);
         
         // set some constants
         this.JUMP_VELOCITY = -700;
         this.SCROLL_SPEED = 5;
+        this.SPEED_MULT = 1;
         this.physics.world.gravity.y = 2600; 
 
         // background
@@ -58,6 +60,8 @@ class Play extends Phaser.Scene {
             groundTile.body.allowGravity = false;
             groundTile2.body.immovable = true;
             groundTile2.body.allowGravity = false;
+            groundTile.setDepth(1);
+            groundTile2.setDepth(1);
             this.ground.add(groundTile);
             this.ground.add(groundTile2);
         }
@@ -65,13 +69,13 @@ class Play extends Phaser.Scene {
 
         // create car obj
         this.car = this.physics.add.sprite(game.config.width, 455 ,  'car').setOrigin(0);
-        this.hood = this.physics.add.sprite(game.config.width+3, 450 ,  'car').setOrigin(0);
-        this.hood.setSize(60,5);
+        this.hood = this.physics.add.sprite(game.config.width+30, 445 ,  'car').setOrigin(0.5,0.5);
+        this.hood.setSize(50,60);
         this.hood.setDisplaySize(60,5);
         this.car.body.allowGravity = false;
         this.hood.body.allowGravity = false;
         this.hood.body.immovable = true;
-        this.ground.add(this.hood);
+        
 
         // meteor obj
         this.meteor =  this.physics.add.sprite(game.config.width, Math.random()*(425-380)+380,  'meteor').setOrigin(0);
@@ -79,8 +83,10 @@ class Play extends Phaser.Scene {
         
         // add enemies to group
         this.enemy = this.add.group();
+        this.meteors = this.add.group();
         this.enemy.add(this.car);
         this.enemy.add(this.meteor);
+        this.meteors.add(this.meteor)
         
         
         
@@ -110,7 +116,7 @@ class Play extends Phaser.Scene {
             this.player.setVelocityY(0);
             this.p1Health--;
         });
-        this.physics.add.collider(this.meteor, this.player, ()=>{
+        this.physics.add.collider(this.meteors, this.player, ()=>{
             this.trash.alpha= 0;
             this.meteor_reset();
             if(this.p1Health>1){
@@ -122,6 +128,10 @@ class Play extends Phaser.Scene {
             this.player.setVelocityX(0);
             this.player.setVelocityY(0);
             this.p1Health--;
+        });
+        this.physics.add.collider(this.player, this.hood, ()=>{
+            this.player.body.setVelocityY(0);
+            this.player.body.setVelocityX(-1*this.hood.body.velocity.x+300);
         });
 
         // game over variable 
@@ -165,24 +175,45 @@ class Play extends Phaser.Scene {
         this.time.addEvent({ delay: 2500, callback: this.miles, callbackScope: this, loop: true });
 
         if(highScore>=0){
-            this.add.text(game.config.width-8, 12, "High Score: "+highScore+" mi", dropshadow).setOrigin(1,0);
-            this.add.text(game.config.width-10, 10, "High Score: "+highScore+" mi", textConfig).setOrigin(1,0);
+            this.highScoreShadow = this.add.text(game.config.width-8, 12, "High Score: "+highScore+" mi", dropshadow).setOrigin(1,0);
+            this.highScoreDisplay = this.add.text(game.config.width-10, 10, "High Score: "+highScore+" mi", textConfig).setOrigin(1,0);
 
         // powerup spawn
         this.time.addEvent({ delay: 10000, callback: this.shieldSpawn, callbackScope: this, loop: true });
         }
-        this.bgm.setLoop(true);
-        this.bgm.play();
+       // this.bgm.setLoop(true);
+        //this.bgm.play();
+
+        this.nighttime = this.time.delayedCall(15000, () => {
+            this.player.setDepth(1);
+            this.meteor.setDepth(1);
+            this.car.setDepth(1);
+            this.hood.setDepth(1);
+            this.shield.setDepth(1);
+            this.trash.setDepth(1);
+            this.scoreRightDropshadow.setDepth(1);
+            this.scoreRight.setDepth(1);
+            this.highScoreShadow.setDepth(1);
+            this.highScoreDisplay.setDepth(1);
+            this.cityscape.setDepth(0);
+            this.cityscape = this.add.tileSprite(0, 0, game.config.width, game.config.height, 'city2').setOrigin(0);
+            this.cityscape.setDepth(0);
+            this.SCROLL_SPEED +=10;
+            this.SPEED_MULT = 1.5;
+            }, null, this);
+            this.state="behind"
+
     }
 
         
     update() {
+        console.log("Player X \: "+this.player.x);
         // update tile sprites (tweak for more "speed")
 
         // game ending handling
         if(this.gameOver){
-            this.bgm.setLoop(false);
-        this.bgm.stop();
+            //this.bgm.setLoop(false);
+        //this.bgm.stop();
             if(this.score>highScore){
                 highScore = this.score;
             }
@@ -193,28 +224,45 @@ class Play extends Phaser.Scene {
         // what to do in game runtime
         if(!this.gameOver){
                 
-
-            // this.car.body.setVelocityX(-300);
-            // this.meteor.body.setVelocityX(-400);
-
-
             //make bg scroll
-            this.cityscape.tilePositionX += this.SCROLL_SPEED;
+            this.cityscape.tilePositionX += this.SCROLL_SPEED; 
             //this.groundTile += this.SCROLL_SPEED;
 
             //check for player touching ground
             this.player.onGround = this.player.body.touching.down;
 
             // FIXME: player position can exceed 50 if ducking while velocity.x > 0
+
+            if(this.player.x<50){
+                this.state = "behind";
+            
+            }else if(this.player.x>70){
+               this.state = "ahead";
+            }
+            else{
+                this.state = "zone";
+            }
+            
             if(this.player.onGround){
                 this.jump = 1;
                 this.jumping=false;
-                if(this.player.x<50){
-                    this.player.body.velocity.x += 1;
-                }else{
-                    this.player.body.velocity.x = 0;
-                }
+                
+                
+            
             }
+            console.log("PLAYER STATE "+this.state)
+            switch(this.state){
+                case "behind":
+                    this.player.body.velocity.x =20;
+                    break;
+                case "ahead":
+                    this.player.body.velocity.x =-20;
+                    break;
+                case "zone":
+                    this.player.body.velocity.x = 0;
+
+            }
+          
             
 
 
@@ -243,6 +291,9 @@ class Play extends Phaser.Scene {
                 this.meteor_reset();
             }
             
+            if((this.meteor.tilePositionX-this.car.tilePositionX)>-100||(this.car.tilePositionX-this.meteor.tilePositionX)>-100){
+                this.meteor.body.velocity.x+=30;
+            }
 
             // controlling the sliding
             if(keyDOWN.isDown){
@@ -255,9 +306,10 @@ class Play extends Phaser.Scene {
                 this.player.angle = 90;
                 this.player.setSize(65,40);
                 this.player.setDisplaySize(45,70);
+                
                 if(!this.jumping){
-                this.player.y = 467.5;
-                this.player.body.setVelocityY(0);
+                // this.player.y = 467.5;
+                this.player.body.setVelocityY(700);
                 }
             }
 
@@ -271,7 +323,7 @@ class Play extends Phaser.Scene {
                 this.player.y = 450;
                 this.player.setSize(40,65);
                 this.player.setDisplaySize(45,70);
-                // this.player.x = 70;
+
             }
 
 
@@ -319,10 +371,14 @@ class Play extends Phaser.Scene {
 
     // reset the meteor 
     meteor_reset(){
+        this.meteor.destroy();
+        this.meteor =  this.physics.add.sprite(game.config.width, Math.random()*(425-380)+380,  'meteor').setOrigin(0);
+        this.meteor.body.allowGravity = false;
         this.meteor.x = game.config.width+50;
         this.meteor.setVelocityY(0);
-        this.meteor.setVelocityX(-1*((Math.random()*(500-400)+400)));
+        this.meteor.setVelocityX((-1*((Math.random()*(500-400)+400)))*this.SPEED_MULT);
         this.meteor.y = Math.random()*(425-380)+380;
+        this.meteors.add(this.meteor);
 
     }
 
@@ -330,9 +386,9 @@ class Play extends Phaser.Scene {
     //reset the car
     car_reset(){
         this.car.x = game.config.width+50;
-        this.num = -1*((Math.random()*(500-300)+300))
+        this.num = (-1*((Math.random()*(500-300)+300)))*this.SPEED_MULT;
         this.car.body.setVelocityX(this.num);
-        this.hood.x = game.config.width+55;
+        this.hood.x = this.car.x+30;
         this.hood.body.setVelocityX(this.num);
 
     }
